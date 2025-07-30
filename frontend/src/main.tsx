355e8449ddf9b61540fa32c5e4270d40954f1857
boweryjg <jasonwilliamgolden@gmail.com>
2025-07-30 07:00:40 -0400

    Remove SVG debug code that was causing error spam

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ErrorBoundary } from './components/ErrorBoundary'
-import { debugSVGCircles } from './utils/debugSVG'
+// import { debugSVGCircles } from './utils/debugSVG'
 
-// Start debugging SVG circles
-debugSVGCircles();
+// Start debugging SVG circles - DISABLED TO STOP ERROR SPAM
+// debugSVGCircles();
 
-// Override console.error to catch SVG errors
-const originalError = console.error;
-console.error = (...args) => {
-  if (args[0]?.includes?.('attribute cy: Expected length')) {
-    console.trace('SVG Circle Error Stack Trace:');
-  }
-  originalError.apply(console, args);
-};
+// Override console.error to catch SVG errors - DISABLED
+// const originalError = console.error;
+// console.error = (...args) => {
+//   if (args[0]?.includes?.('attribute cy: Expected length')) {
+//     console.trace('SVG Circle Error Stack Trace:');
+//   }
+//   originalError.apply(console, args);
+// };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
